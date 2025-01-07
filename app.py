d5e55bd5724d18da201b0f0d63cf7dd9ebaa2963
Anubhab Halder <116893402+anubhabhalder18@users.noreply.github.com>
2025-01-07 11:37:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from selenium.webdriver.common.by import By
 def remove_text(text, phrase):
     return text.replace(phrase, '')
 
-
+# Go to booking.com search for the location in stay and copy the url and paste it here
 webpage = "https://www.booking.com/searchresults.html?ss=Dehradun&ssne=Dehradun&ssne_untouched=Dehradun&label=vienna-PNVY5BTMRTTJHYXTpLjJCAS553249712605%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-1365191499%3Alp9298496%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YZVcNNsENnH02-pWD53qm9c&aid=306395&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-2094211&dest_type=city&checkin=2025-01-09&checkout=2025-01-10&group_adults=2&no_rooms=1&group_children=0"
 
-
+#change filename to ur liking 
 filename = "hotels_in_dehradun.xlsx"
 
 
